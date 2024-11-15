a4a50f76f0240797c28e4cc43efc77721b2fc074
Demute <dev@demute.studio>
2024-11-15 18:51:27 +0100

    added fmod test scripts and more fmod notes

---
+++ FmodScripts/AiSound Test.js
@@ -0,0 +1,29 @@
+﻿// AiSound Test.js
+// This script creates a test event and lists the declared function in the Scripts tab
+
+studio.menu.addMenuItem({
+    name: "AiSound Test",
+    isEnabled: true,
+    execute: createEventWithnestedEvents,
+});
+
+function createEventWithnestedEvents() {
+    const mainEvent = studio.project.create("Event");
+    mainEvent.name = "v16";
+    var event = mainEvent;
+
+    for (var i = 0; i < 4; i++) {
+        const track = event.masterTrack;
+        const instrument = track.addSound(event.timeline, "EventSound", 0, 10);
+        
+        event = instrument.event;
+    }
+    
+    //instrument.relationships.dump();
+    
+}
+
+
+
+
+
--- Notes AiSound Obsidian/FMOD/Notes.md
+++ Notes AiSound Obsidian/FMOD/Notes.md
@@ -1,1 +1,6 @@
-- Can use stings for labeled parameters : setParameterByNameWithLabel
+https://alessandrofama.com/tutorials/fmod/fmod-studio/scripting-api-basics
+- Can use strings for labeled parameters : `setParameterByNameWithLabel` in unity for example
+- If you want to create a menu item, you need to use `studio.menu.addMenuItem`
+	- To be able to launch it from the editor don't include the `()` to your function in `execute`
+	- If you do, it will launch it on `reload` and be greyed out
+- For some reason, Fmod hates `let` and requires instead `var`
