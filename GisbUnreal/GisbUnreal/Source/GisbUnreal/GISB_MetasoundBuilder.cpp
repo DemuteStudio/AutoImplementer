54e8862f7f058119498ef01ce14d5b427af7d419
Mathéo Gollas <mgollas@student.heaj.be>
2025-05-09 17:02:23 +0200

    Metasound builder setup

--- GisbUnreal/GisbUnreal/Source/GisbUnreal/GISB_MetasoundBuilder.cpp
+++ GisbUnreal/GisbUnreal/Source/GisbUnreal/GISB_MetasoundBuilder.cpp
@@ -5,21 +5,113 @@
 #include "MetasoundSource.h"
 #include "MetasoundFrontend.h"
 #include "MetasoundFrontendDocument.h"
+#include "MetasoundBuilderSubsystem.h"
+#include "MetasoundBuilderBase.h"
 #include "AssetRegistry/AssetRegistryModule.h"
+#include "MetasoundEditorSubsystem.h"
 
 UMetaSoundSource* UGISB_MetasoundBuilder::CreateMetasoundFromGISB(UGisbImportContainerBase* gisb, const FString& Name, const FString& path)
 {
-	UPackage* MSPackage = CreatePackage(*path);
-	UMetaSoundSource* GeneratedMS = NewObject<UMetaSoundSource>(MSPackage, *Name, RF_Public | RF_Standalone);
-	GeneratedMS->Modify();
-	FAssetRegistryModule::AssetCreated(GeneratedMS);
+	bool canLoop = false;
+	bool shouldStereo = false;
+	DetectLoopAndMono(gisb, canLoop, shouldStereo);
+	EMetaSoundOutputAudioFormat format = shouldStereo ? EMetaSoundOutputAudioFormat::Stereo : EMetaSoundOutputAudioFormat::Mono;
+	
+	EMetaSoundBuilderResult result;
+	UMetaSoundBuilderSubsystem* BuilderSubsystem = GEngine->GetEngineSubsystem<UMetaSoundBuilderSubsystem>();
+	FMetaSoundBuilderNodeOutputHandle OnPlayNode;
+	FMetaSoundBuilderNodeInputHandle OnFinishedNode;
+	TArray<FMetaSoundBuilderNodeInputHandle> outAudioHandles;
+	UMetaSoundSourceBuilder* builder = BuilderSubsystem->CreateSourceBuilder(FName(Name), OnPlayNode, OnFinishedNode, outAudioHandles, result,format, !canLoop);
+	builder->SetAuthor("GISB");
+	
+	SetupNodes();
+	ConnectContainerToGraph(builder, gisb, &OnPlayNode, &OnFinishedNode, &outAudioHandles);
 
-	//Do stuff here
-
-	MSPackage->MarkPackageDirty();
-	const FString AssetMSFileName = FPackageName::LongPackageNameToFilename(path, FPackageName::GetAssetPackageExtension());
-	UPackage::SavePackage(MSPackage, GeneratedMS, EObjectFlags::RF_Public | EObjectFlags::RF_Standalone, *AssetMSFileName);
+	FMetaSoundBuilderOptions options = FMetaSoundBuilderOptions();
+	UMetaSoundEditorSubsystem* MySubsystem = GEditor->GetEditorSubsystem<UMetaSoundEditorSubsystem>();
+	TScriptInterface<IMetaSoundDocumentInterface> ms = MySubsystem->BuildToAsset(builder, "GISB", Name, path, result);
+	if (result == EMetaSoundBuilderResult::Failed)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Failed to build UMetaSoundSource."))
+	}
+	UObject* Object = ms.GetObject();
+	UMetaSoundSource* GeneratedMS = Cast<UMetaSoundSource>(Object);
 	return GeneratedMS;
 }
 
+void UGISB_MetasoundBuilder::SetupNodes()
+{
+	
+}
 
+void UGISB_MetasoundBuilder::DetectLoopAndMono(UGisbImportContainerBase* gisb, bool& canLoop, bool& shouldStereo)
+{
+	if (canLoop && shouldStereo) return;
+
+	UGisbImportContainerSimpleSound* simplesound = Cast<UGisbImportContainerSimpleSound>(gisb);
+	if (simplesound != nullptr)
+	{
+		if(simplesound->loop) canLoop = true;
+		if(simplesound->SoundWave->NumChannels > 1) shouldStereo = true;
+	}
+	
+	UGisbImportContainerBlend* blend = Cast<UGisbImportContainerBlend>(gisb);
+	if (blend != nullptr)
+	{
+		for (UGisbImportContainerBase* child : blend->SoundArray)
+		{
+			DetectLoopAndMono(child, canLoop, shouldStereo);
+		}
+	}
+
+	UGisbImportContainerRandom* random = Cast<UGisbImportContainerRandom>(gisb);
+	if (random != nullptr)
+	{
+		for (UGisbImportContainerBase* child : random->SoundArray)
+		{
+			DetectLoopAndMono(child, canLoop, shouldStereo);
+		}
+	}
+
+	UGisbImportContainerSwitch* swtch = Cast<UGisbImportContainerSwitch>(gisb);
+	if (swtch != nullptr)
+	{
+		for (auto& child : swtch->SoundDictionary)
+		{
+			DetectLoopAndMono(child.Value, canLoop, shouldStereo);
+		}
+	}
+}
+
+void UGISB_MetasoundBuilder::ConnectContainerToGraph(UMetaSoundSourceBuilder* builder, UGisbImportContainerBase* gisb, FMetaSoundBuilderNodeOutputHandle* executionHandle, FMetaSoundBuilderNodeInputHandle* finishHandle, TArray<FMetaSoundBuilderNodeInputHandle>* audioHandles)
+{
+	UGisbImportContainerSimpleSound* simplesound = Cast<UGisbImportContainerSimpleSound>(gisb);
+	if (simplesound != nullptr)
+	{
+		//EMetaSoundBuilderResult result;
+		//builder->AddNodeByClassName(WavePlayerMonoNode, result); // Uncomment when WavePlayerMonoNode is set up
+		// Create simplesound node at the beginning and at the end
+	}
+
+	UGisbImportContainerBlend* blend = Cast<UGisbImportContainerBlend>(gisb);
+	if (blend != nullptr)
+	{
+		// Create blend node at the beginning and at the end
+		// Replicate for each child
+	}
+
+	UGisbImportContainerRandom* random = Cast<UGisbImportContainerRandom>(gisb);
+	if (random != nullptr)
+	{
+		// Create random node at the beginning and at the end
+		// Replicate for each child
+	}
+
+	UGisbImportContainerSwitch* swtch = Cast<UGisbImportContainerSwitch>(gisb);
+	if (swtch != nullptr)
+	{
+		// Create switch node at the beginning and at the end
+		// Replicate for each child
+	}
+}
--- GisbUnreal/GisbUnreal/Source/GisbUnreal/GISB_MetasoundBuilder.h
+++ GisbUnreal/GisbUnreal/Source/GisbUnreal/GISB_MetasoundBuilder.h
@@ -5,6 +5,11 @@
 #include "CoreMinimal.h"
 #include "MetasoundSource.h"
 #include "GisbImportContainerBase.h"
+#include "GisbImportContainerSimpleSound.h"
+#include "GisbImportContainerBlend.h"
+#include "GisbImportContainerRandom.h"
+#include "GisbImportContainerSwitch.h"
+#include "MetasoundBuilderSubsystem.h"
 #include "GISB_MetasoundBuilder.generated.h"
 
 /**
@@ -18,4 +23,21 @@ public:
 	
 	UFUNCTION(BlueprintCallable, Category = "Metasound")
 	static UMetaSoundSource* CreateMetasoundFromGISB(UGisbImportContainerBase* gisb, const FString& Name, const FString& path);
+
+private:
+	static FMetasoundFrontendClassName WavePlayerMonoNode;
+	static FMetasoundFrontendClassName WavePlayerStereoNode;
+	static FMetasoundFrontendClassName GisbRandomNode;
+	static FMetasoundFrontendClassName GisbSwitchNode;
+	static FMetasoundFrontendClassName GisbProbabilityNode;
+	static FMetasoundFrontendClassName GisbVolumeNode;
+	static FMetasoundFrontendClassName GisbPitchNode;
+	static FMetasoundFrontendClassName GisbLowpassNode;
+	static FMetasoundFrontendClassName GisbAttenuationNode;
+	static TArray<FMetasoundFrontendClassName> MixerNodes;
+	static TArray<FMetasoundFrontendClassName> TriggerAnyNodes;
+	static TArray<FMetasoundFrontendClassName> TriggerAccumulateNodes;
+	static void SetupNodes();
+	static void DetectLoopAndMono(UGisbImportContainerBase* gisb, bool& shouldLoop, bool& shouldStereo);
+	static void ConnectContainerToGraph(UMetaSoundSourceBuilder* builder, UGisbImportContainerBase* gisb, FMetaSoundBuilderNodeOutputHandle* executionHandle, FMetaSoundBuilderNodeInputHandle* finishHandle, TArray<FMetaSoundBuilderNodeInputHandle>* audioHandles);
 };
