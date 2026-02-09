b8bd1a15baf0bbdc892c9aa3b57842d45b09e69f
Robin - Demute <robin@demute.studio>
2026-02-09 12:03:03 +0100

    changes to acomodate new local structure

--- .releases/Unreal 5.7/GISB_Importer/Source/GISB_Importer/GisbSoundBankDataAsset.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "GisbSoundBankDataAsset.h"
-
-FPrimaryAssetId UGisbSoundBankDataAsset::GetPrimaryAssetId() const 
-{
-	return FPrimaryAssetId(AssetType, GetFName());
-}
--- .releases/Unreal 5.7/GISB_Importer/Source/GISB_Importer/Private/ISXProjectSettings.cpp
+++
@@ -1,8 +0,0 @@
-#include "ISXProjectSettings.h"
-
-UISXProjectSettings::UISXProjectSettings()
-{
-	CategoryName = TEXT("Plugins");
-	SectionName = TEXT("ISX Settings");
-	DistanceScale = 1000.0f;
-}
